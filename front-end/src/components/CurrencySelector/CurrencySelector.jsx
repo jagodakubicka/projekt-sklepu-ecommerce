db27ca3e1ada2e645e192c6eab54f4fb5197d023
Jagoda Kubicka <93054999+jagodakubicka@users.noreply.github.com>
2023-12-07 18:01:56 +0100

    localStorage as new hook

--- front-end/src/components/CurrencySelector/CurrencySelector.jsx
+++ front-end/src/components/CurrencySelector/CurrencySelector.jsx
@@ -11,7 +11,7 @@ export function CurrencySelector(){
   value ={currency}
   onChange={(e)=> {
    setCurrency(e.currentTarget.value);
-   localStorage['selected_currency'] = e.currentTarget.value
+
   }} 
   className={styles.currencySelector}>
    <option value={CURRENCIES.PLN}>{CURRENCIES.PLN}</option>
